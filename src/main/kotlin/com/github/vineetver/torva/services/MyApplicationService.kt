914771e58abe497d7733318ba2dc6d61c98b0f7c
GitHub Action <action@github.com>
2022-08-23 04:15:08 +0000

    Template cleanup

--- src/main/kotlin/com/github/vineetver/torva/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/vineetver/torva/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.vineetver.torva.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.vineetver.torva.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/vineetver/torva/services/MyProjectService.kt
+++ src/main/kotlin/com/github/vineetver/torva/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.vineetver.torva.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.vineetver.torva.MyBundle
 
 class MyProjectService(project: Project) {
 
